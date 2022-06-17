e53591963c618cf64bf567fa9a209d4a4cf90eaf
marion-mcg <104576130+marion-mcg@users.noreply.github.com>
2022-06-17 12:05:41 +0100

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The functions below allow us to calculate the inverse of a square matrix and cache that result
+## As the assignment states, "assume that the matrix supplied is always invertible" we are only working with square matrices
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+## makeCacheMatrix creates a special object, which can be used to return the inverse of an already calculated matrix
+makeCacheMatrix<-function(x = matrix()) {
+    m <- NULL
+	#set the value of the vector
+    set <- function (y) {
+        x <<- y
+        m <<- NULL
+    }
+	#get the value of the vector
+    get <-function() x
+	#set the value of the inverse
+    setmatrix <- function(solve) m<<- solve
+	
+	#get the value of the inverse
+    getmatrix <- function() m
+    list (set=set, get=get, setmatrix=setmatrix, getmatrix=getmatrix)
 }
 
+## the following function calculates the inverse, and sets the value of the inverse in the cache
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve<-function(x) {
+	#check to see if the inverse has already been calculated
+    m <- x$getmatrix()
+    if(!is.null(m)){
+		#if not NULL, return from cache
+        message("getting cached data")
+        return(m)
+    }
+	#Else, calculate the inverse of the data
+    data <- x$get()
+    m <- solve(data)
+	#Set the value of the inverse in the cache via the setmatrix function
+    x$setmatrix(m)
+    m
 }
